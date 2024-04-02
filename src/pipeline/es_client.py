8641c1e95e84ab136f12b0fc25ea510b1d4cc032
debija <42231132+JohanAndresen@users.noreply.github.com>
2024-04-02 16:53:50 +0200

    gui-search delay&retry now in es-client, not dockercompose.yml

--- src/pipeline/es_client.py
+++ src/pipeline/es_client.py
@@ -1,22 +1,37 @@
 
 import logging
 from elasticsearch import Elasticsearch
+import time
 import json
 
 logging.basicConfig(level=logging.INFO)
 
 class Search:
     def __init__(self):
+
         self.es = Elasticsearch('http://host.docker.internal:9200')
-        #, retry_on_timeout=True) 
-        #, http_compress=True)
-        #, request_timeout=30)
-        client_info = self.es.info()
-        logging.info("Connected to Elasticsearch!")
-        logging.info(json.dumps(client_info.body, indent = 4))
+
+        retries=0
+        max_retries=12
+        
+        while retries < max_retries:
+            try:
+                # time.sleep(10)
+                client_info = self.es.info()
+                logging.info("Connected to Elasticsearch!")
+                logging.info(json.dumps(client_info.body, indent = 4))
+                break
+            except Exception as e:
+                time.sleep(10)
+                retries += 1
+                logging.info(f"Waiting {retries}/{max_retries} 10 more seconds for Elasticsearch...")
+                if retries == max_retries:
+                    raise e
 
 
 # https://www.elastic.co/search-labs/tutorials/search-tutorial/full-text-search/connect-python#connect-to-a-self-hosted-elasticsearch-docker-container
 # https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/config.html#timeouts
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/cat-health.html
 # https://elasticsearch-py.readthedocs.io/en/v8.13.0/async.html
+
+      
